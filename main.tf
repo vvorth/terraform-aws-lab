9dd02214dcca0fad4edcaa5d5a8b31a601f4a7d8
Andrey Latipov <dark.vvorth@gmail.com>
2026-03-03 16:21:40 +0500

    remove unneded nat gateway

--- main.tf
+++ main.tf
@@ -109,23 +109,23 @@ resource "aws_internet_gateway" "internet_gateway" {
 }
 
 #Create EIP for NAT Gateway
-resource "aws_eip" "nat_gateway_eip" {
-  #  domain     = "vpc"
-  depends_on = [aws_internet_gateway.internet_gateway]
-  tags = {
-    Name = "demo_igw_eip"
-  }
-}
+# resource "aws_eip" "nat_gateway_eip" {
+#   #  domain     = "vpc"
+#   depends_on = [aws_internet_gateway.internet_gateway]
+#   tags = {
+#     Name = "demo_igw_eip"
+#   }
+# }
 
 #Create NAT Gateway
-resource "aws_nat_gateway" "nat_gateway" {
-  depends_on    = [aws_subnet.public_subnets]
-  allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
-  tags = {
-    Name = "demo_nat_gateway"
-  }
-}
+# resource "aws_nat_gateway" "nat_gateway" {
+#   depends_on    = [aws_subnet.public_subnets]
+#   allocation_id = aws_eip.nat_gateway_eip.id
+#   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+#   tags = {
+#     Name = "demo_nat_gateway"
+#   }
+# }
 
 
 resource "random_string" "random" {
