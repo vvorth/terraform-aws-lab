014ff8a331a3f29fbf88dc3853bc6ed67eea8873
Andrey Latipov <dark.vvorth@gmail.com>
2026-03-03 16:28:23 +0500

    move all outputs to optuts.tf

--- main.tf
+++ main.tf
@@ -294,22 +294,3 @@ module "s3_bucket" {
 
 
 
-
-output "public_ip_server_subnet_1" {
-  value = "web_server public ip: ${aws_instance.web_server.public_ip}"
-}
-
-output "public_dns_server_subnet_1" {
-  value = "web_server public dns: ${aws_instance.web_server.public_dns}"
-}
-
-output "public_ip_server_2_subnet_2" {
-  value = "web_server_2 public ip: ${aws_instance.web_server_2.public_ip}"
-}
-
-output "public_dns_server_2_subnet_2" {
-  value = "web_server_2 public dns: ${aws_instance.web_server_2.public_dns}"
-}
-
-
-
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
+
+
+
 output "environment" {
   value = var.environment
 }
@@ -5,3 +8,25 @@ output "environment" {
 output "workspaces" {
   value = terraform.workspace
 }
+
+
+
+
+output "public_ip_server_subnet_1" {
+  value = "web_server public ip: ${aws_instance.web_server.public_ip}"
+}
+
+output "public_dns_server_subnet_1" {
+  value = "web_server public dns: ${aws_instance.web_server.public_dns}"
+}
+
+output "public_ip_server_2_subnet_2" {
+  value = "web_server_2 public ip: ${aws_instance.web_server_2.public_ip}"
+}
+
+output "public_dns_server_2_subnet_2" {
+  value = "web_server_2 public dns: ${aws_instance.web_server_2.public_dns}"
+}
+
+
+
