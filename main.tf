f5db809e016e1ab5673dd0651776836930230496
Andrey Latipov <dark.vvorth@gmail.com>
2026-03-03 16:24:11 +0500

    remove private subnets

--- main.tf
+++ main.tf
@@ -33,17 +33,17 @@ resource "aws_vpc" "vpc" {
 }
 
 #Deploy the private subnets
-resource "aws_subnet" "private_subnets" {
-  for_each          = var.private_subnets
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
-  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+# resource "aws_subnet" "private_subnets" {
+#   for_each          = var.private_subnets
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+#   availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
 
-  tags = {
-    Name      = each.key
-    Terraform = "true"
-  }
-}
+#   tags = {
+#     Name      = each.key
+#     Terraform = "true"
+#   }
+# }
 
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
@@ -72,18 +72,18 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "private_route_table" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    # gateway_id     = aws_internet_gateway.internet_gateway.id
-    nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-  tags = {
-    Name = "demo_private_rtb"
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     # gateway_id     = aws_internet_gateway.internet_gateway.id
+#     nat_gateway_id = aws_nat_gateway.nat_gateway.id
+#   }
+#   tags = {
+#     Name = "demo_private_rtb"
+#   }
+# }
 
 #Create route table associations
 resource "aws_route_table_association" "public" {
@@ -93,12 +93,12 @@ resource "aws_route_table_association" "public" {
   subnet_id      = each.value.id
 }
 
-resource "aws_route_table_association" "private" {
-  depends_on     = [aws_subnet.private_subnets]
-  route_table_id = aws_route_table.private_route_table.id
-  for_each       = aws_subnet.private_subnets
-  subnet_id      = each.value.id
-}
+# resource "aws_route_table_association" "private" {
+#   depends_on     = [aws_subnet.private_subnets]
+#   route_table_id = aws_route_table.private_route_table.id
+#   for_each       = aws_subnet.private_subnets
+#   subnet_id      = each.value.id
+# }
 
 #Create Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
@@ -108,24 +108,24 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
-#Create EIP for NAT Gateway
-resource "aws_eip" "nat_gateway_eip" {
-  #  domain     = "vpc"
-  depends_on = [aws_internet_gateway.internet_gateway]
-  tags = {
-    Name = "demo_igw_eip"
-  }
-}
+# #Create EIP for NAT Gateway
+# resource "aws_eip" "nat_gateway_eip" {
+#   #  domain     = "vpc"
+#   depends_on = [aws_internet_gateway.internet_gateway]
+#   tags = {
+#     Name = "demo_igw_eip"
+#   }
+# }
 
-#Create NAT Gateway
-resource "aws_nat_gateway" "nat_gateway" {
-  depends_on    = [aws_subnet.public_subnets]
-  allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
-  tags = {
-    Name = "demo_nat_gateway"
-  }
-}
+# #Create NAT Gateway
+# resource "aws_nat_gateway" "nat_gateway" {
+#   depends_on    = [aws_subnet.public_subnets]
+#   allocation_id = aws_eip.nat_gateway_eip.id
+#   subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+#   tags = {
+#     Name = "demo_nat_gateway"
+#   }
+# }
 
 
 resource "random_string" "random" {
