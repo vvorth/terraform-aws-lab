149013c2a9da693a2cbb853fd84609fea8880fba
Andrey Latipov <dark.vvorth@gmail.com>
2026-03-03 16:29:21 +0500

    remove unneeded private route table dependant on nat gateway

--- main.tf
+++ main.tf
@@ -93,12 +93,12 @@ resource "aws_route_table_association" "public" {
   subnet_id      = each.value.id
 }
 
-resource "aws_route_table_association" "private" {
-  depends_on     = [aws_subnet.private_subnets]
-  route_table_id = aws_route_table.private_route_table.id
-  for_each       = aws_subnet.private_subnets
-  subnet_id      = each.value.id
-}
+# resource "aws_route_table_association" "private" {
+#   depends_on     = [aws_subnet.private_subnets]
+#   route_table_id = aws_route_table.private_route_table.id
+#   for_each       = aws_subnet.private_subnets
+#   subnet_id      = each.value.id
+# }
 
 #Create Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
